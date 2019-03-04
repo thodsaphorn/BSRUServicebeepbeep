28b4f95432279016ebc6da1a2ae6449e479549f4
thodsaphorn <thodsaphornmaiket@hotmail.com>
2019-03-04 14:51:46 +0700

    Register Success

---
+++ app/src/main/java/thodsaphorn/maiket/ac/th/bsruservicebeepbeep/MyConstant.java
@@ -0,0 +1,11 @@
+package thodsaphorn.maiket.ac.th.bsruservicebeepbeep;
+
+public class MyConstant {
+
+
+    private String urlAddUser = "http://www.androidthai.in.th/bsru/addUserBEEP.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
