eb0744fc8a7cc2048e1459855ac8052d71042c4a
thodsaphorn <thodsaphornmaiket@hotmail.com>
2019-03-04 10:19:44 +0700

    Toolbar Success

--- app/src/main/java/thodsaphorn/maiket/ac/th/bsruservicebeepbeep/RegisterFragment.java
+++ app/src/main/java/thodsaphorn/maiket/ac/th/bsruservicebeepbeep/RegisterFragment.java
@@ -2,22 +2,45 @@ package thodsaphorn.maiket.ac.th.bsruservicebeepbeep;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
 
-    public RegisterFragment() {
-        // Required empty public constructor
+        //Create Toolbar
+        createToolbar();
+
+    } //Main Method
+
+    private void createToolbar() {
+        Toolbar toolbar = getActivity().findViewById(R.id.toolbarRegister);
+        ((MainActivity) getActivity()).setSupportActionBar(toolbar);
+
+        //Setup Title
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle("Please Fill Every Blank");
+
+        //Setup Navigation Icon
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
